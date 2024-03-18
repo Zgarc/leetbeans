8424a2b93848f615cfc84fd95a1c49830e6f1364
Zachary <zgarc025@fiu.edu>
2024-03-18 18:36:17 -0400

    add base case for L238

--- src/test/java/L238/TestCases.java
+++ src/test/java/L238/TestCases.java
@@ -26,6 +26,16 @@ public class TestCases {
     }
 
     @Test
+    public void allZeros() {
+        int[] input = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
+
+        int[] expected = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
+        int[] actual = L238.productExceptSelf(input);
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
     public void test1() {
         int[] input = new int[]{1, 2, 3, 4};
 
