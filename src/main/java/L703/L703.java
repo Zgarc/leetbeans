07b7e941fafd3120f0d96a89e56f99887c701aac
Zachary <zgarc025@fiu.edu>
2024-03-20 19:28:46 -0400

    L703

---
+++ src/main/java/L703/L703.java
@@ -0,0 +1,24 @@
+package L703;
+
+import java.util.PriorityQueue;
+
+public class L703 {
+
+    final PriorityQueue<Integer> heap = new PriorityQueue<>();
+    final int k;
+
+    public L703(int k, int[] nums) {
+        this.k = k;
+        for (int n : nums) add(n);
+    }
+
+    public int add(int val) {
+        if (heap.size() < k) {
+            heap.offer(val);
+        } else if (val > heap.peek()) {
+            heap.poll();
+            heap.add(val);
+        }
+        return heap.peek();
+    }
+}
---
+++ src/test/java/L703/TestCases.java
@@ -0,0 +1,4 @@
+package L703;
+
+public class TestCases {
+}
