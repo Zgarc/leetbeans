bbfe2f115ed46401d7ffe68da16a305991956b1f
Zachary <zgarc025@fiu.edu>
2024-03-22 20:42:07 -0400

    add test for L703

--- src/test/java/L703/TestCases.java
+++ src/test/java/L703/TestCases.java
@@ -14,4 +14,22 @@ public class TestCases {
         assertEquals(kthLargest.add(9), 8);   // return 8
         assertEquals(kthLargest.add(4), 8);   // return 8
     }
+
+    @Test
+    public void test2() {
+        L703 kthLargest = new L703(3, new int[]{3, 7, 8, 1});
+        // first they get sorted:
+        // 1, 3, 7, 8
+
+        // 1, 3, 3, 7, 8
+        assertEquals(kthLargest.add(3), 3);
+        // 1, 3, 3, 5, 7, 8
+        assertEquals(kthLargest.add(5), 5);
+        // 1, 3, 3, 5, 7, 8, 10
+        assertEquals(kthLargest.add(10), 7);
+        // 1, 3, 3, 5, 7, 8, 9, 10
+        assertEquals(kthLargest.add(9), 8);
+        // 1, 3, 3, 4, 5, 7, 8, 9, 10
+        assertEquals(kthLargest.add(4), 8);
+    }
 }
