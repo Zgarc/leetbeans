402e79b2212787e9365e63a28f195c8f0294f8c9
Zachary Garcia <zgarc025@fiu.edu>
2024-03-24 21:34:48 -0400

    add basecases to L121

--- src/test/java/L121/TestCases.java
+++ src/test/java/L121/TestCases.java
@@ -14,6 +14,14 @@ public class TestCases {
     }
 
     @Test
+    public void oneElementArray() {
+        int[] prices = new int[]{1};
+        int maxProfit = L121.maxProfit(prices);
+
+        assertEquals(0, maxProfit);
+    }
+
+    @Test
     public void test1() {
         int[] prices = new int[]{7, 1, 5, 3, 6, 4};
         int maxProfit = L121.maxProfit(prices);
