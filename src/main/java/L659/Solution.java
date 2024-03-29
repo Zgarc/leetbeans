4160a65a389425cf9bc0f7a67a3743bf30eb1ae0
Zachary <zgarc025@fiu.edu>
2024-03-28 22:57:35 -0400

    L659

--- src/main/java/L659/Solution.java
+++ src/main/java/L659/Solution.java
@@ -1,22 +1,28 @@
 package L659;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
-    /*
-     * @param strs: a list of strings
-     * @return: encodes a list of strings to a single string.
-     */
     public static String encode(List<String> strs) {
-        // write your code here
-        return "";
+        StringBuilder encodedString = new StringBuilder();
+        for (String str : strs) {
+            encodedString.append(str.length()).append("#").append(str);
+        }
+        return encodedString.toString();
     }
 
-    /*
-     * @param str: A string
-     * @return: decodes a single string to a list of strings
-     */
     public static List<String> decode(String str) {
-        return List.of();
+        List<String> list = new ArrayList<>();
+        int i = 0;
+        while (i < str.length()) {
+            int j = i;
+            while (str.charAt(j) != '#') j++;
+
+            int length = Integer.parseInt(str.substring(i, j));
+            i = j + 1 + length;
+            list.add(str.substring(j + 1, i));
+        }
+        return list;
     }
 }
--- src/test/java/L659/TestCases.java
+++ src/test/java/L659/TestCases.java
@@ -5,7 +5,8 @@ import java.util.List;
 public class TestCases {
     @org.junit.Test
     public void test1() {
-        final String encodedString = Solution.encode(List.of("Hey", "There", "My", "Name", "Is", "Jon"));
-        assert encodedString.isEmpty();
+        List<String> strings = List.of("Hey", "There", "My", "Name", "Is", "Jon");
+        final String encodedString = Solution.encode(strings);
+        assert Solution.decode(encodedString).equals(strings);
     }
 }
